67e5075797b9a5a7ca867f9736f46ace16f6fe61
Mykola-Vynnychuk <nik4779@gmail.com>
2020-08-03 21:02:58 +0300

    added buttons

--- js/task-01.js
+++ js/task-01.js
@@ -1,24 +1,28 @@
-const Account = function({login, email}) {
-  this.login = login;
-  this.email = email;
-}
+document.getElementById('task-one').addEventListener('click', () => {
+  class Account {
+    constructor({ login, email }) {
+      this.login = login;
+      this.email = email;
+    }
 
-Account.prototype.getInfo = function() {
-  console.log(`Login: ${this.login}, Email: ${this.email}`);
-}
+    getInfo() {
+      console.log(`Login: ${this.login}, Email: ${this.email}`);
+    }
+  }
 
-console.log(Account.prototype.getInfo); // function
+  console.log(Account.prototype.getInfo); // function
 
-const mango = new Account({
-  login: 'Mangozedog',
-  email: 'mango@dog.woof',
+  const mango = new Account({
+    login: 'Mangozedog',
+    email: 'mango@dog.woof',
+  });
+
+  mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+  const poly = new Account({
+    login: 'Poly',
+    email: 'poly@mail.com',
+  });
+
+  poly.getInfo(); // Login: Poly, Email: poly@mail.com
 });
-
-mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-
-const poly = new Account({
-  login: 'Poly',
-  email: 'poly@mail.com',
-});
-
-poly.getInfo(); // Login: Poly, Email: poly@mail.com
